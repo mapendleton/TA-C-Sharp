13ae77856b08ac58bf7f446f50d6a66279f4e26e
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-21 16:10:19 +0700

    Added constructor for Deck Class - finish part 3

--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -13,6 +13,11 @@ namespace BlackJack
 
             Deck deck = new Deck();
 
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
             //Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
             Console.ReadLine();
         }
