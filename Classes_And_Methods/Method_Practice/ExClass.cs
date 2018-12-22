78b475e01cc4e30544c3ef903fc767190b96aeb5
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-22 14:20:34 +0700

    add method practice exercise

---
+++ Classes_And_Methods/Method_Practice/ExClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Practice
+{
+    public class ExClass
+    {
+        public void SomeMath(int num1, int num2)
+        {
+            Console.WriteLine(num1 * 20);
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ Classes_And_Methods/Method_Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExClass exClass = new ExClass();
+            exClass.SomeMath(5, 5);
+            exClass.SomeMath(num1: 3, num2: 4);
+
+            Console.Read();
+        }
+    }
+}
