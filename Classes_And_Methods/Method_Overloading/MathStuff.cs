1831656068c1caa31c3749c70cccb51fab1aa48b
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-22 16:07:34 +0700

    add method overloading exercise

---
+++ Classes_And_Methods/Method_Overloading/MathStuff.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    public class MathStuff
+    {
+        public int DivideByFour(int num)
+        {
+            return num / 4;
+        }
+
+        public decimal DivideByFour(decimal dec)
+        {
+            return dec / 4m;
+        }
+
+        public int DivideByFour(string strNum)
+        {
+            bool success = int.TryParse(strNum, out int num);
+            if (success)
+            {
+                return num / 4;
+            }
+            else
+            {
+                Console.WriteLine("incorrect input returning 0");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Classes_And_Methods/Method_Overloading/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathStuff mathStuff = new MathStuff();
+            Console.WriteLine(mathStuff.DivideByFour(5));
+            Console.WriteLine(mathStuff.DivideByFour(5m));
+            Console.WriteLine(mathStuff.DivideByFour("5"));
+            Console.Read();
+        }
+    }
+}
