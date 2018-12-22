d75fbbc0549b433ea0040d530140648b1bd13acf
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-22 13:43:23 +0700

    add class and methods exercise

---
+++ Classes_And_Methods/Classes_And_Methods/Methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods
+{
+    public class Methods
+    {
+        public static int Squared(int numInput)
+        {
+            int result = numInput * numInput;
+            return result;
+        }
+
+        public static int Cubed(int numInput)
+        {
+            int result = numInput * numInput * numInput;
+            return result;
+        }
+
+        public static float MinTipCalc(float bill)
+        {
+            float result = bill * .10f;
+            return result;
+        }
+    }
+}
---
+++ Classes_And_Methods/Classes_And_Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type a number");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("your number squared: " + Methods.Squared(input));
+
+            Console.WriteLine("your number cubed: " + Methods.Cubed(input));
+
+            Console.WriteLine("your minimum tip: " + Methods.MinTipCalc((float)input));
+
+            Console.Read();
+        }
+    }
+}
