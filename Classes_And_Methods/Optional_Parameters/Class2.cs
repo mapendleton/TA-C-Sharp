bffef753b757800db82a4713134d9b802b257a55
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-22 16:35:33 +0700

    added optional parameter exercise

---
+++ Classes_And_Methods/Optional_Parameters/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters
+{
+    public class Class2
+    {
+        public int DoSomeMath(int num1,int num2 = 1)
+        {
+            return num1 * num2 * 100;
+        }
+    }
+}
---
+++ Classes_And_Methods/Optional_Parameters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 class2 = new Class2();
+            Console.WriteLine("enter num 1");
+            int input1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter optional num 2");
+            string input2 = Console.ReadLine();
+
+            if (input2 == "")
+            {
+                Console.WriteLine(class2.DoSomeMath(input1));
+            }
+            else if (input2 != "")
+            {
+                int intNum = int.Parse(input2);
+                Console.WriteLine(class2.DoSomeMath(input1, intNum));
+            }
+
+            Console.Read();
+        }
+    }
+}
