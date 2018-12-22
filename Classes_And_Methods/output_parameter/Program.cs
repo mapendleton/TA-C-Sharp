af89a786588a239597a85d35e2b2baa131d819c9
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-22 22:06:26 +0700

    added output parameter exercise

---
+++ Classes_And_Methods/output_parameter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace output_parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //step 2
+            OutputClass outputClass = new OutputClass();
+
+            //step 3
+            Console.WriteLine("enter number");
+            int input = int.Parse(Console.ReadLine());
+            outputClass.DivideByTwo(input, out int result);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Classes_And_Methods/output_parameter/outputClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace output_parameter
+{
+    //step 6
+    public static class OutputClass
+    {
+        //step 1 and 4
+        public static void DivideByTwo(int input, out int result)
+        {
+            result = input / 2;
+        }
+
+        //step 5
+        public static void DivideByTwo(float input, out float result)
+        {
+            result = input / 2;
+        }
+
+    }
+}
