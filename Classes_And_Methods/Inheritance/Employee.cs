1ef0a5ee8335c41fe5daf7bf26d2a55d0c96917b
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-23 17:27:25 +0700

    added Interface exercise

--- Classes_And_Methods/Inheritance/Employee.cs
+++ Classes_And_Methods/Inheritance/Employee.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    public class Employee : Person
+    public class Employee : Person, IQuittable
     {
         public int Id { get; set; }
 
+        public void Quit()
+        {
+            Console.WriteLine("{0} {1} id: {2} has quit",this.FirstName,this.LastName,this.Id);
+        }
     }
 }
--- Classes_And_Methods/Inheritance/Program.cs
+++ Classes_And_Methods/Inheritance/Program.cs
@@ -13,7 +13,11 @@ namespace Inheritance
             Employee employee1 = new Employee() { FirstName = "Sample", LastName = "Student" };
 
             employee1.SayName();
+            employee1.Id = 1001;
+            employee1.Quit();
 
+            IQuittable employee2 = new Employee() {FirstName = "Bob", LastName ="Hob",Id = 1002};
+            employee2.Quit();
             Console.Read();
         }
     }
