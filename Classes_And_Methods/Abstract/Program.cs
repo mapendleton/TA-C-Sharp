52d8ee4b752d4020e855873df956d61204d35b89
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-23 16:48:06 +0700

    add abstraction exercise

---
+++ Classes_And_Methods/Abstract/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { firstName = "Sample", lastName = "Student" };
+            employee1.SayName();
+            Console.Read();
+        }
+    }
+}
