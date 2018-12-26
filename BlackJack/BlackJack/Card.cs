d7ea06790e2660704df0f3342f926266eb4d9b28
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-26 13:53:24 +0700

    made Card class into a Struct

--- BlackJack/BlackJack/Card.cs
+++ BlackJack/BlackJack/Card.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlackJack
 {
-    public class Card
+    public struct Card
     {
         public Suit Suit { get; set; }
         public Face Face { get; set; }
