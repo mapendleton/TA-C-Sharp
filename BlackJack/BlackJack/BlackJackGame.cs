5fa81e4f6a1f835e04cc9557b84f7d47c9607198
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-26 16:30:20 +0700

    add BlackJackDealer class that inherits from Dealer class - started Main class code

--- BlackJack/BlackJack/BlackJackGame.cs
+++ BlackJack/BlackJack/BlackJackGame.cs
@@ -8,9 +8,15 @@ namespace BlackJack
 {
     public class BlackJackGame : Game, IWalkAway
     {
+        public BlackJackDealer Dealer { get; set; }
+
         public override void Play()
         {
+            Dealer = new BlackJackDealer();
+            foreach (Player player in Players)
+            {
 
+            }
         }
 
         public override void ListPlayer()
--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -11,15 +11,27 @@ namespace BlackJack
         static void Main(string[] args)
         {
 
-            Deck deck = new Deck();
-            deck.Shuffle();
-
-            foreach (Card card in deck.Cards)
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("And how much money did you bring today?");
+            int bank = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of blackjack right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+                Player player = new Player(playerName, bank);
+                Game game = new BlackJackGame();
+                game += player;
+                player.IsActivelyPlaying = true;
+                while(player.IsActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
             }
-            Console.WriteLine(deck.Cards.Count);
-            Console.ReadLine();
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
         }
 
     }
