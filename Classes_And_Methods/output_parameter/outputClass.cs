1a033f5e241e298b7aa803dc9b6548d23a35bb35
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-27 10:03:17 +0700

    delete static from Output class

--- Classes_And_Methods/output_parameter/outputClass.cs
+++ Classes_And_Methods/output_parameter/outputClass.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 namespace output_parameter
 {
     //step 6
-    public static class OutputClass
+    public class OutputClass
     {
         //step 1 and 4
-        public static void DivideByTwo(int input, out int result)
+        public void DivideByTwo(int input, out int result)
         {
             result = input / 2;
         }
 
         //step 5
-        public static void DivideByTwo(float input, out float result)
+        public void DivideByTwo(float input, out float result)
         {
             result = input / 2;
         }
