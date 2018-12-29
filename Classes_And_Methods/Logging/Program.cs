f83a4b4ba807f4e1db532ce1d0bfc05f1d00a358
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-29 09:46:20 +0700

    added logging exercise

---
+++ Classes_And_Methods/Logging/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            string input = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:/users/Matthew Pendleton/documents/Tech Academy Files/ExerciseLogs/log.txt",true))
+            {
+                file.WriteLine(input);
+            }
+
+            string result = File.ReadAllText(@"C:/users/Matthew Pendleton/documents/Tech Academy Files/ExerciseLogs/log.txt");
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
