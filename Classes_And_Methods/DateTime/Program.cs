fd350316679b58115c743e1b77159f7824721857
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-30 18:56:42 +0700

    added dateTime exercise

---
+++ Classes_And_Methods/DateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("enter number");
+            double hours = double.Parse(Console.ReadLine());
+            DateTime dateTime = DateTime.Now.AddHours(hours);
+            Console.WriteLine(dateTime);
+            Console.Read();
+        }
+    }
+}
