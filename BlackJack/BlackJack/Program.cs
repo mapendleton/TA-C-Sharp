5a81217313b55ce17017ad5a92c621c9213522f6
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-30 22:03:58 +0700

    added some exception handling

--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -16,8 +16,17 @@ namespace BlackJack
 
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = int.Parse(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("please enter digits only. No decimals");
+            }
+
+
             Console.WriteLine("Hello, {0}. Would you like to join a game of blackjack right now?", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
@@ -33,7 +42,22 @@ namespace BlackJack
                 player.IsActivelyPlaying = true;
                 while(player.IsActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out!");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("an error occurred. Please contact your system administrator");
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
--- BlackJack/Casino/BlackJackGame.cs
+++ BlackJack/Casino/BlackJackGame.cs
@@ -22,11 +22,21 @@ namespace Casino.BlackJack
             Dealer.Stay = false;
             Dealer.Deck = new Deck();
             Dealer.Deck.Shuffle();
-            Console.WriteLine("Place your bet");
 
             foreach (Player player in Players)
             {
-                int bet = int.Parse(Console.ReadLine());
+                bool validAnswer = false;
+                int bet = 0;
+                while (!validAnswer)
+                {
+                    Console.WriteLine("Place your bet");
+                    validAnswer = int.TryParse(Console.ReadLine(), out bet);
+                    if(!validAnswer) Console.WriteLine("Please use digits only. No decimals");
+                }
+                if (bet < 0)
+                {
+                    throw new FraudException();
+                }
                 bool successfullyBet = player.Bet(bet);
                 if (!successfullyBet)
                 {
