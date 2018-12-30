54d7c0b88c13d0cf5a4841d7000c693c9c1144e8
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-30 20:02:32 +0700

    added additional features exercise

---
+++ Classes_And_Methods/Additional_Features/Chain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    class Chain
+    {
+        public Chain(string title) : this(title, 0)
+        {
+        }
+        public Chain(string title,int year)
+        {
+            Title = title;
+            Year = year;
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("The title: {0} and the year: {1}",Title,Year);
+        }
+    }
+}
---
+++ Classes_And_Methods/Additional_Features/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string title = "Hello World";
+            var exChain = new Chain(title, 1999);
+            exChain.PrintInfo();
+            Console.Read();
+        }
+    }
+}
