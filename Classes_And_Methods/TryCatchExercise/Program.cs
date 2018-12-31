af057512ac3417f113c00b9ba4c1086732929c8f
Matthew Pendleton <mapendleton37@gmail.com>
2018-12-31 13:37:24 +0700

    added try catch exercise - create own exception

---
+++ Classes_And_Methods/TryCatchExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter your age");
+                int age = int.Parse(Console.ReadLine());
+                if (age <= 0)
+                {
+                    throw new ZeroOrBelowException("enter an age above 0");
+                }
+
+                int currentYear = DateTime.Now.Year;
+                int yearBorn = currentYear - age;
+                Console.WriteLine("Year born: {0}",yearBorn);
+                Console.Read();
+            }
+            catch (ZeroOrBelowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something else went wrong");
+                Console.Read();
+                return;
+            }
+            
+        }
+    }
+}
---
+++ Classes_And_Methods/TryCatchExercise/ZeroOrBelowException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchExercise
+{
+    public class ZeroOrBelowException : Exception
+    {
+        public ZeroOrBelowException()
+        {
+        }
+        public ZeroOrBelowException(string message)
+            : base(message)
+        {
+        }
+        public ZeroOrBelowException(string message,Exception inner)
+            : base(message,inner)
+        {
+        }
+    }
+}
